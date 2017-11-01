283c32831c57de5c604112dd768e97d6fdfa5679
Timur Kuschanov <timur.kuschanov@yandex.ru>
2017-11-02 00:43:07 +0300

    second commit

--- src/main/java/ru/nt202/controller/Controller.java
+++ src/main/java/ru/nt202/controller/Controller.java
@@ -22,6 +22,15 @@ public class Controller implements Initializable {
     private LineChart<?, ?> HumanVision;
 
     @FXML
+    private LineChart<?, ?> BlackBody3200;
+
+    @FXML
+    private LineChart<?, ?> IRFilter;
+
+    @FXML
+    private LineChart<?, ?> Sensor;
+
+    @FXML
     private CategoryAxis x;
 
     @FXML
@@ -41,8 +50,7 @@ public class Controller implements Initializable {
             series1.getData().add(new XYChart.Data(xAxis, yAxis));
         }
 
-//        String filename3200 = CSVReader.class.getResource("/3200.csv").getPath();
-        String filename3200 = CSVReader.class.getResource("/IMX265LQR_BLUE.csv").getPath();
+        String filename3200 = CSVReader.class.getResource("/3200.csv").getPath();
         CSVReader csvReader2 = new CSVReader();
         csvReader2.loadFile(filename3200);
         XYChart.Series series2 = new XYChart.Series();
@@ -52,11 +60,42 @@ public class Controller implements Initializable {
             series2.getData().add(new XYChart.Data(xAxis, yAxis));
         }
 
-        series1.setName("Кривая видимости зрительного анализатора");
-        HumanVision.setStyle("CHART_COLOR_1: #FFFF00");
-        series2.setName("Спектральные характеристики источника белого (3200 K)");
-        HumanVision.getData().addAll(series1, series2);
 
+        String filenameIRFilter = CSVReader.class.getResource("/BG40.csv").getPath();
+        CSVReader csvReader3 = new CSVReader();
+        csvReader3.loadFile(filenameIRFilter);
+        XYChart.Series series3 = new XYChart.Series();
+        for (int i = 0; i < csvReader3.getLength(); i++) {
+            String xAxis = String.valueOf(csvReader3.getX().get(i));
+            Double yAxis = csvReader3.getY().get(i);
+            series3.getData().add(new XYChart.Data(xAxis, yAxis));
+        }
+
+        String filenameSensor = CSVReader.class.getResource("/IMX265LQR_GREEN.csv").getPath();
+        CSVReader csvReader4 = new CSVReader();
+        csvReader4.loadFile(filenameSensor);
+        XYChart.Series series4 = new XYChart.Series();
+        for (int i = 0; i < csvReader4.getLength(); i++) {
+            String xAxis = String.valueOf(csvReader4.getX().get(i));
+            Double yAxis = csvReader4.getY().get(i);
+            series4.getData().add(new XYChart.Data(xAxis, yAxis));
+        }
+
+        HumanVision.getData().addAll(series1);
+        HumanVision.setStyle("CHART_COLOR_1: #B57BFF;");
+
+        BlackBody3200.getData().addAll(series2);
+        BlackBody3200.setStyle("CHART_COLOR_1: #000000;");
+
+        IRFilter.getData().addAll(series3);
+        IRFilter.setStyle("CHART_COLOR_1: #FF0000;");
+
+        Sensor.getData().addAll(series3);
+        Sensor.setStyle("CHART_COLOR_1: #00FF00;");
+
+//        HumanVision.setStyle("CHART_COLOR_2: #282725;");
+
+//        HumanVision.getStylesheets().add("/ChartLineStyle.css");
     }
 
 }
